12fc05444b47f73564d79d85f194f51c603061ea
INWYTIA <inwytia@gmail.com>
2020-03-07 18:33:31 -0600

    added connection to database

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleT = require("console.table");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "employee_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  readColleges();
+});
